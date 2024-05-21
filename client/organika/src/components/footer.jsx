c3807e5e10b57c8bfc4622e2c985edb0d8443fe1
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2024-05-20 19:56:09 -0700

    52022024

---
+++ client/organika/src/components/footer.jsx
@@ -0,0 +1,15 @@
+// imports
+
+// logic
+function Footer()
+{
+    return(
+
+        <div>
+            <label>Organika Store a very expensive one</label>
+        </div>
+    );
+}
+
+// the export
+export default Footer;
---
+++ client/organika/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+//The imports 
+import "./navbar.css";
+
+//The logic 
+function Navbar()
+{
+    return(
+    <div>
+        menu goes here!
+        <btn class="btn btn-sm btn-primary">btn</btn>
+    </div>
+    );
+}
+//the export
+export default Navbar;
+//Mini challenge
+// Create your own component for the footer, 
+// and render it below the navbar
